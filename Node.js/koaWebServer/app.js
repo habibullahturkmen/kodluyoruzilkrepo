13b8ebf4f2661df26e607650f1ccd2426a9bfdff
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-10-29 19:35:21 +0430

    Ödev 6 - Koa.js ile Web Sunucusu Yazımı

---
+++ Node.js/koaWebServer/app.js
@@ -0,0 +1,34 @@
+const koa = require('koa');
+const koaRouter = require('koa-router');
+const fs = require('fs');
+
+const app = new koa();
+const router = new koaRouter();
+
+router.get('/', (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('index.html');
+});
+
+router.get('/index.html', (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('index.html');
+});
+
+router.get('/urunlerimiz.html', (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('urunlerimiz.html');
+});
+
+router.get('/Hakkimizda.html', (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('Hakkimizda.html');
+});
+
+// Router middleware !important
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("koa server started on port " + port);
+});
