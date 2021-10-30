efb65ca48b83f20d004363ff8fb81d57025e0625
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-10-30 20:39:29 +0430

    express and nodemon added

--- Node.js/PCAT/app.js
+++ Node.js/PCAT/app.js
@@ -1,3 +1,17 @@
-const fn = function() {
-    console.log("lelolo")
-}
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 2,
+        name: "photo name",
+        description: "photo description"
+    }
+    res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
