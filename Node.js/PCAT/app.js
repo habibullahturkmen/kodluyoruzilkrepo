412ddf5b13da1ecf2758beaf07b03c9d405f4ec8
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-11-06 20:41:55 +0430

    Model and dinamic content added

--- Node.js/PCAT/app.js
+++ Node.js/PCAT/app.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+
 const path = require('path');
 
+const Photo = require('./models/Photos');
+
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Execute Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos:photos
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -21,6 +36,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+   await Photo.create(req.body);
+   res.redirect('/');
 });
 
 const port = 3000;
