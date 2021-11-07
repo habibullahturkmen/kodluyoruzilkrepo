fd555e527aadd29b9bfd1cf703cb7ecf48523f94
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-11-07 15:16:35 +0430

    Tekil Fotoğraf Sayfaları eklendi

--- Node.js/PCAT/app.js
+++ Node.js/PCAT/app.js
@@ -30,6 +30,15 @@ app.get('/', async (req, res) => {
     });
 });
 
+app.get('/photos/:id', async (req, res) => {
+    // console.log(req.param.id);
+    // res.render('about');
+    const photo = await Photo.findById(req.params.id);
+    res.render('photo', {
+        photo:photo
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about');
 });
