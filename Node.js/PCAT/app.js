282c91b30f166cfcc458911f60de14ddc9b5acd9
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-11-07 20:39:57 +0430

    Upload image and Update image added

--- Node.js/PCAT/app.js
+++ Node.js/PCAT/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-
 const path = require('path');
-
+const fs = require('fs');
 const Photo = require('./models/Photos');
 
 const app = express();
@@ -21,18 +22,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 // Routes
 app.get('/', async (req, res) => {
-    const photos = await Photo.find({});
+    const photos = await Photo.find({}).sort('-dateCreated');
     res.render('index', {
         photos:photos
     });
 });
 
 app.get('/photos/:id', async (req, res) => {
-    // console.log(req.param.id);
-    // res.render('about');
     const photo = await Photo.findById(req.params.id);
     res.render('photo', {
         photo:photo
@@ -48,8 +49,38 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-   await Photo.create(req.body);
-   res.redirect('/');
+
+    const uploadDir = 'public/uploads';
+
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+    }
+
+    let uploadedImage = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+    
+    uploadedImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadedImage.name
+        });
+        res.redirect('/');
+    });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+    const photo = await Photo.findOne({_id: req.params.id});
+    res.render('edit', {
+        photo:photo
+    });
+});
+
+app.put('/photos/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id });
+    photo.title = req.body.title;
+    photo.description = req.body.description;
+    photo.save();
+    res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = 3000;
